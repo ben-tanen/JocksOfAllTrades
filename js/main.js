cc5479b31172defa624baafa6155aaa2c4f3a831
Ben Tanen <ben.tanen@gmail.com>
2015-02-26 16:46:24 -0500

    added scrolling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
     $('.nav li').click(function() {
+        var divs = ["services", "testimonials", "about-us", "contact"]
+
         $('.nav li').removeClass('active');
         $(this).addClass('active');
+
+        console.log(divs[$(this).index()]);
+        $('html, body').animate({
+            scrollTop: $("#" + divs[$(this).index()]).offset().top
+        }, $(this).index()*100 + 500);
     });
 
     userguide_tooltip = new jBox('Tooltip',{
